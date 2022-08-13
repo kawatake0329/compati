6f8cff6a4565665c5c99bf7727df840e5ce660ae
EC2 Default User <ec2-user@ip-172-31-7-148.ap-northeast-1.compute.internal>
2022-08-13 08:59:09 +0000

    [add]header追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,30 @@
 Rails.application.routes.draw do
 
-  root to: 'public/homes#top'
 
-  devise_for :admins, controllers: {
-    sessions: "admin/sessions"
+  root to: 'public/homes#top'
+  get "public/home/about"=>"public/homes#about"
+
+  namespace :public do
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+    get "customer/mypage"=>"customers#show"
+    resource :customers, only: [:edit, :update]
+  end
+
+  namespace :master do
+    resources :posts, only: [:index, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+  devise_for :masters,controllers: {
+    sessions: "master/sessions"
   }
 
   devise_for :customers, controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions: 'public/sessions',
+    passwords: 'public/passwords'
   }
+  devise_scope :user do
+    post 'public/guest_sign_in', to: 'public/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_101142) do
+ActiveRecord::Schema.define(version: 2022_08_13_052833) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -63,6 +63,18 @@ ActiveRecord::Schema.define(version: 2022_08_11_101142) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "masters", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_masters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_masters_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"

fead68b68ebcd3dcc00d8910aa491716aa0f167d
EC2 Default User <ec2-user@ip-172-31-7-148.ap-northeast-1.compute.internal>
2022-08-29 13:35:40 +0000

    [Fix]master側ログイン画面

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+master = Master.create!(
+  [
+    {email: 'lucas@test.com', password: 'password'}
+  ]
+)

0eb1198e788e791d8fd5b4c0ee630ffe7107192b
EC2 Default User <ec2-user@ip-172-31-7-148.ap-northeast-1.compute.internal>
2022-08-29 13:03:49 +0000

    [Fix]退会機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   before_action :customer_state, only:[:create]
+
   def after_sign_in_path_for(resource)
      public_posts_path
   end
@@ -15,8 +16,19 @@ class Public::SessionsController < Devise::SessionsController
     sign_in customer
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
-  # before_action :configure_sign_in_params, only: [:create]
 
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+     if @customer.valid_password?(params[:customer][:password]) && (@customer.deleted_user == false)
+      flash[:error] = "退会済みです。"
+      redirect_to root_path
+     else
+      flash[:error] = "必須項目を入力してください。"
+     end
+    end
+  end
+  # before_action :configure_sign_in_params, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -32,18 +44,8 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
-  def customer_state
-    @customer = Customer.find_by(email: params[:customer][:email])
-    if @customer
-     if @customer.valid_password?(params[:customer][:password]) && (@customer.deleted_user == false)
-      flash[:error] = "退会済みです。"
-      redirect_to root_path
-     else
-      flash[:error] = "必須項目を入力してください。"
-     end
-    end
-  end
+  # protected
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
